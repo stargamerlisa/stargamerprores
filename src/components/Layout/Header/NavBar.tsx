274f25e332980e1780f157e3ba9717b5ffdb4dae
stargamerlisa <130560012+stargamerlisa@users.noreply.github.com>
2023-05-09 10:02:00 +0200

    Update NavBar.tsx

--- src/components/Layout/Header/NavBar.tsx
+++ src/components/Layout/Header/NavBar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "👤 about me", to: "/about" }]
+  const links = [{ id: 1, name: "", to: "/about" }]
   return (
     <div className="flex-shrink-0">
       <ul className="flex flex-row">
