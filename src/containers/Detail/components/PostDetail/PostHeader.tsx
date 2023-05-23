5b99b61b33c834bda7cbe89f38023e221416301b
stargamerlisa <130560012+stargamerlisa@users.noreply.github.com>
2023-05-23 16:49:41 +0200

    Update PostHeader.tsx

--- src/containers/Detail/components/PostDetail/PostHeader.tsx
+++ src/containers/Detail/components/PostDetail/PostHeader.tsx
@@ -50,7 +50,7 @@ const PostHeader: React.FC<Props> = ({ data }) => {
             )}
           </div>
           {data.thumbnail && (
-            <div className="relative w-full pb-[66%] lg:pb-[50%] bg-gray-200 dark:bg-zinc-700 mb-7 rounded-none overflow-visible">
+            <div className="relative w-full pb-[100%] lg:pb-[100%] bg-gray-200 dark:bg-zinc-700 mb-7 rounded-none overflow-visible">
               <Image
                 src={data.thumbnail}
                 className="object-cover"
